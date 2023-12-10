875ffa8a020c579816d7db00e87d9365ba002932
Denny Rezvanov <128595610+dennymann2@users.noreply.github.com>
2023-12-10 03:15:44 +0100

    Update scripts.js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,24 +1,16 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu-icon').addEventListener('click', function() {
+        var nav = document.querySelector('.main-nav');
+        nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+    });
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.style.opacity = '1';
-    document.body.style.transition = 'opacity 2s ease-in-out';
-});
-
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    var nav = document.querySelector('.main-nav');
-    if (nav.style.right === '0px') {
-        nav.style.right = '-250px';
-        nav.style.opacity = '0';
-    } else {
-        nav.style.right = '0px';
-        nav.style.opacity = '1';
-    }
 });
